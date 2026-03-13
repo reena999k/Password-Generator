d977a0d7c87b76fa1e49089db71504c3d53e27d6
reena999k <reenakushwaha84509@gmail.com>
2026-03-14 00:18:26 +0530

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,16 @@
+# VOICE ASSISTANT
+# Developed by: Reena Kushwaha
+# Internship Project - Oasis Infobyte
+import random
+import string
+
+length = int(input("Enter password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+print("Generated Password:", password)
